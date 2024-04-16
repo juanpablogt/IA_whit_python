c0e0bdcd9e5e71fe8442004a67048a7f84b4f444
JPablo <juapabgonzalez@utp.edu.co>
2024-04-15 21:36:34 -0500

    Practicas

---
+++ promedios.py
@@ -0,0 +1,8 @@
+A=float(input())
+B=float(input())
+
+A= A*3.5 
+B= B*7.5
+
+MEDIA= (A+B)/11
+
---
+++ salarty.py
@@ -0,0 +1,7 @@
+nombre=input()
+A=float(input())
+B=float(input())
+
+TOTAL = A+(B*0.15)
+
+print("TOTAL = R$ {0:.2f}".format(TOTAL))
